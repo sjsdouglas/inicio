7cbd77f5f2060aad6d756fd8d7791aeedc68fe2a
Douglas <104078248+sjsdouglas@users.noreply.github.com>
2025-01-24 23:36:14 -0300

    att

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-irPara('menu/certificados.html');
-
-function irPara (pagina) {
-    if (pagina === '#') {
-        return;
-    }
-    fetch(pagina)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao carregar a página');   
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.querySelector('.conteudo').innerHTML = data;
-        });
+function irPara(pagina) {
+    document.querySelector('iframe').src = pagina;
 }
